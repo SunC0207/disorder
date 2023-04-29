0a1dcd6e15e54dda3289969caf9441b7aa6009e1
SungC <a9517538246z@gmail.com>
2023-04-29 18:09:01 +0800

    新建

---
+++ src/main/java/com/disorder/DisorderApplication.java
@@ -0,0 +1,13 @@
+package com.disorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisorderApplication.class, args);
+	}
+
+}
