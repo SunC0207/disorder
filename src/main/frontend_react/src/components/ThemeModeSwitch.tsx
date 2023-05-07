9fd41ca2a2cd1d9a2d38e50072a6617983fe4a07
SungC <a9517538246z@gmail.com>
2023-05-07 11:56:58 +0800

    主題切換

---
+++ src/main/frontend_react/src/components/ThemeModeSwitch.tsx
@@ -0,0 +1,18 @@
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+
+const ThemeModeSwitch = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
+
+  return (
+    <HStack>
+      <Switch
+        colorScheme="yellow"
+        isChecked={colorMode === "dark"}
+        onChange={toggleColorMode}
+      />
+      <Text>黑夜模式</Text>
+    </HStack>
+  );
+};
+
+export default ThemeModeSwitch;
